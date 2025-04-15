ea411c596f5a975b38dc2b9f41c5b8a3d2b9806f
Admin <admin@emergiotech.com>
2025-04-15 17:21:48 +0530

    urls.py update

--- emergio/urls.py
+++ emergio/urls.py
@@ -3,15 +3,16 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('emergio/units/course',CourseView.as_view()),
-    path('emergio/units/placement',PlacementView.as_view()),
-    path('emergio/units/team',TeamView.as_view()),
-    path('emergio/units/news',NewsView.as_view()),
-    path('emergio/units/story',StoryView.as_view()),
-    path('emergio/units/career',CareerView.as_view()),
-    path('emergio/units/application',AppView.as_view()),
-    path('emergio/units/game',GameView.as_view()),
-    path('emergio/units/contact',ContactView.as_view()),
-    path('emergio/units/form',FormView.as_view()),
-    path('emergio/units/lead',LeadView.as_view()),
+    path('emergio/course/', CourseView.as_view()),
+    path('emergio/placement/', PlacementView.as_view()),
+    path('emergio/team/', TeamView.as_view()),  # fixed typo
+    path('emergio/news/', NewsView.as_view()),
+    path('emergio/story/', StoryView.as_view()),
+    path('emergio/career/', CareerView.as_view()),
+    path('emergio/application/', AppView.as_view()),
+    path('emergio/game/', GameView.as_view()),
+    path('emergio/contact/', ContactView.as_view()),
+    path('emergio/form/', FormView.as_view()),
+    path('emergio/lead/', LeadView.as_view()),
 ]
+
