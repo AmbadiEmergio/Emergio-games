69491db033961f774d86792dfc2ccd9f9c030678
Admin <admin@emergiotech.com>
2025-04-15 15:55:46 +0530

    newfile added

--- emergio/utils/csv_util.py
+++ emergio/utils/csv_util.py
@@ -7,10 +7,19 @@ def create_csv(data):
     csv_buffer = io.StringIO()
     csv_writer = csv.writer(csv_buffer)
 
-    # Write the header row
-    if data and len(data) > 0:
-        headers = data[0].keys()
-        csv_writer.writerow(headers)
+    today = datetime.date.today().strftime('%Y-%m-%d')
+    filename = f'leads_{today}.csv'
+
+    if Leads.objects.filter(file__contains=filename).exists():
+        existing_leads = Leads.objects.get(file__contains=filename)
+        existing_content = existing_leads.file.read().decode('utf-8')
+        csv_buffer.write(existing_content)
+    else:
+
+        # Write the header row
+        if data and len(data) > 0:
+            headers = data[0].keys()
+            csv_writer.writerow(headers)
 
     # Write the data rows
     for row in data:
@@ -19,4 +28,4 @@ def create_csv(data):
     csv_content = csv_buffer.getvalue()
 
     leads = Leads.objects.create()
-    leads.file.save('leads.csv', ContentFile(csv_content), save=True)
+    leads.file.save(filename, ContentFile(csv_content), save=True)
